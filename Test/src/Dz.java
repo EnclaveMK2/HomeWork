63c820212710bd6de165d0cd4a3b171be5aeec8a
EnclaveMK2 <koenigtiger503@gmail.com>
2022-02-02 18:54:51 +0200

    Some changes

---
+++ Test/src/Dz.java
@@ -0,0 +1,19 @@
+public class Dz {
+    public Character charik = 's';
+    Short shorti = 6;
+    Integer a = 20;
+    Long longi = 5L;
+    Boolean booli = false;
+    Float floti = 657.67f;
+    Double doubli = 678.567;
+
+
+    char some = 'k';
+    short jo = 4;
+    int b = 30;
+    long l = 50;
+    boolean boo = true;
+    float floa = 56.67f;
+    double doub = 67.567;
+
+}
---
+++ Test/src/Some.java
@@ -0,0 +1,9 @@
+public class Some {
+
+    public static void main(String[] args) {
+        String name = "Hello";
+        System.out.println(name);
+        Integer a = 20;
+
+    }
+}
