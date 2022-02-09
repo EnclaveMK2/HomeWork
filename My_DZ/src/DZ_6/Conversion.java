42d589f9d162985d45a32a594b807ae15316b3eb
EnclaveMK2 <koenigtiger503@gmail.com>
2022-02-09 17:27:34 +0200

    DZ_6 Done

--- My_DZ/src/DZ_6/Conversion.java
+++ My_DZ/src/DZ_6/Conversion.java
@@ -5,12 +5,36 @@ import java.util.Scanner;
 public class Conversion {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Please, enter currency, for example Dollar, Euro or Canadian");
-        String currency = input.next();
+        System.out.println("Please, enter currency, for example Dollars, Euros or Canadians");
+        String currency = input.next().toLowerCase();
 
-        switch (currency){
-            case "Dollar":
+        float multiplier;
+        switch (currency) {
+            case "dollars":
+                multiplier = 0.036f;
+                break;
+            case "euros":
+                multiplier = 0.031f;
+                break;
+            case "canadians":
+                multiplier = 0.045f;
+                break;
+            default:
+                multiplier = 0;
         }
+
+        if (multiplier != 0) {
+            System.out.println("Please, enter amount of hryvnias you want to exchange");
+            int hryvnias = input.nextInt();
+
+            System.out.println("You will have " + exchange(hryvnias, multiplier) + " of " + currency);
+        } else {
+            System.out.println("This type of currency is not allowed");
+        }
+    }
+
+    static float exchange(int hryvnias, float multiplier) {
+        return hryvnias * multiplier;
     }
 
 }
