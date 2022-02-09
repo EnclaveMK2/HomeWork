fd88f71ff0e5a1574ebb34159b64a6b7eb291f22
EnclaveMK2 <koenigtiger503@gmail.com>
2022-02-09 13:00:56 +0200

    DZ_6 partially

---
+++ My_DZ/src/DZ_6/Conversion.java
@@ -0,0 +1,16 @@
+package DZ_6;
+
+import java.util.Scanner;
+
+public class Conversion {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please, enter currency, for example Dollar, Euro or Canadian");
+        String currency = input.next();
+
+        switch (currency){
+            case "Dollar":
+        }
+    }
+
+}
