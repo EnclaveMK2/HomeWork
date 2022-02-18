44a8e9d9f2a6ff8aea9745a09805f566eace2d1b
EnclaveMK2 <koenigtiger503@gmail.com>
2022-02-18 11:28:20 +0200

    DZ_7 final fix

--- My_DZ/src/DZ_7/Calculator.java
+++ My_DZ/src/DZ_7/Calculator.java
@@ -1,11 +1,21 @@
 package DZ_7;
 
 public class Calculator {
+    static int divider = 5;
+
     public static void main(String[] args) {
         System.out.println(calculate(5, 10, 25));
     }
 
     static String calculate(int one, int two, int three) {
-        return " " + one / 5 + " " + two / 5 + " " + three / 5;
+       return checkParityAndCalculate(one) + " " + checkParityAndCalculate(two)+ " " + checkParityAndCalculate(three);
+    }
+
+    static String checkParityAndCalculate(int number) {
+        if (number % divider == 0) {
+            return String.valueOf(number / divider);
+        } else {
+            return String.valueOf((double) number / divider);
+        }
     }
 }
