4f68d1a2ce6a1c671a20afb8ccd6aa18eda67bf4
SZaverukha <koenigtiger503@gmail.com>
2022-04-18 12:23:07 +0300

    tag: Title description
    Long multi line
    description here.
    
    Issue: <jira-ticket-id>, <jira-ticket-id> ...

--- My_DZ/src/Essential/DZ_14/Third/Main.java
+++ My_DZ/src/Essential/DZ_14/Third/Main.java
@@ -21,7 +21,6 @@ public class Main {
 
         Arithmetic div = (Integer a, Integer b) -> (a / b);
         System.out.println("Divide = " + div.operation(first, second)); /*проблемі с делением меньшего на большее*/
-
         
         List<Integer> listInt = new ArrayList<>();
         listInt.add(5);
